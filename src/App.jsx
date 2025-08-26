6aa58a6b6a0a599656aa090e6a21f20634aac693
franvelayos <fvelayos01@gmail.com>
2025-08-26 20:09:02 -0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import NavBar from './components/navBar/NavBar'
+
+function App() {
+  
+
+  return (
+    <>
+    
+    <NavBar></NavBar>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navBar/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+const NavBar = () => {
+    //js
+    return (
+        //html
+        <div>
+            <nav class="navbar bg-body-tertiary">
+                <div class="">
+                    
+                    <a class="navbar-brand" href="#">
+                        <img src="../../src/assets/images/TUPBOLOGO.png" alt="" width="95" height="auto" class="d-inline-block align-text-top"/>
+                        <h2>CINETUP</h2>
+                    </a>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar
